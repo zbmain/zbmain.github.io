72983d30dab5cbd35750ad91e56c5c10a3d8fd9d
zbmain <zbmain@qq.com>
2021-04-07 18:15:58 +0800

    update home

---
+++ cssjs/js.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d66b1b7cf0bd11e0a719659f2c2256e0";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
