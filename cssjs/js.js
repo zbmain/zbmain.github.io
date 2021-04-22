b5ee9414383070659d82b9aff7d1ec1b4cdcb1e2
zbmain <zbmain@qq.com>
2021-04-22 08:07:24 +0800

    update cv

--- cssjs/js.js
+++ cssjs/js.js
@@ -1,3 +1,22 @@
+var state = getParamKey('cv') == undefined ? 'none':'block';//初始状态
+//获取参数
+function getParamKey(key){
+    var str = window.location.search;
+    if (str.indexOf(key) != -1) {
+        var pos_start = str.indexOf(key) + key.length + 1;
+        var pos_end = str.indexOf("&", pos_start);
+        if (pos_end == -1) {
+            return str.substring(pos_start)
+        }
+    }
+}
+// cvtips
+function cvTips(){
+  $('#cv-tips').css('display',state);
+  $('#cv-tips').delay(3000).slideUp(1000);
+}
+
+// 百度统计
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
@@ -5,3 +24,7 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+
+// $(function(){
+//     $('#cv-tips').delay(3000).slideUp(500);
+// });
